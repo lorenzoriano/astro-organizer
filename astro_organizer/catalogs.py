1b3e868f6e1c3e0e6982c274feddff041e104c21
lorenzoriano <lorenzo.riano@berkeley.edu>
2012-09-25 14:26:26 -0700

    improved database management and added locations

--- astro_organizer/catalogs.py
+++ astro_organizer/catalogs.py
@@ -1,6 +1,5 @@
 import ephem
 import tables
-import itertools
 import logging
 
 classes_dict = {"A":"Cluster of galaxies",
@@ -37,9 +36,72 @@ class TableBody(tables.IsDescription):
     radius = tables.Float64Col()
     elong = tables.Float64Col()
     edb_string = tables.StringCol(1024)
-    
 
-def create_catalog_from_edb(name, save_db, edb_file_obj):
+class Location(tables.IsDescription):
+    name = tables.StringCol(128)
+    latitude = tables.Float64Col()
+    longitude = tables.Float64Col()
+    height = tables.Float64Col()
+
+class MasterDatabase(object):
+    """This is a class that keeps track of all the info in the organizer. The
+    data is stored in a h5 file"""
+    
+    def __init__(self, database):
+        if type(database) is str:
+            if not database.endswith(".h5"):
+                database += ".h5"
+            filters = tables.Filters(complevel=9)
+            self.db = tables.openFile(database, "a", 
+                                      title="AstroOrganizer Database",
+                                      filters=filters)
+            
+        
+        elif type(database) is tables.File:
+            self.db = database
+        
+        else:
+            raise ValueError("Wrong type for database: %s" % type(database))
+        
+        self.populate_groups()
+    
+    def populate_groups(self):
+        """Create the groups usually stored in the database, if those are not
+        already present."""
+        
+        if "/catalogs" not in self.db:
+            self.db.createGroup("/","catalogs")
+        if "/locations" not in self.db:
+            self.db.createTable("/","locations", Location)        
+        
+        self.db.flush()
+        
+    def load_edb(self, catalog_name, edb_file_obj):
+        """Loads a xephem edb database specified in edb_file_obj and stores it
+        into catalog_name.
+        """
+        create_catalog_from_edb(catalog_name, self, edb_file_obj)
+        
+    def add_location(self, name, latitude, longitude, height):
+        """Adds a location to the database.
+        
+        Parameters:
+        name: a string with the name of the location
+        latitude: floating point number (positive is north)
+        longitude: floating point number (poisitive is east)  
+        height = height in meters
+        """
+        
+        loc_table = self.db.root.locations
+        row = loc_table.row
+        row["name"] = name
+        row["latitude"] = latitude
+        row["longitude"] = longitude
+        row["height"] = height
+        row.append()
+        loc_table.flush()
+
+def create_catalog_from_edb(name, master_db, edb_file_obj,):
     """Creates an h5 catalog from a xepeh edb one.
     The xephem database format is described at 
     ttp://www.clearskyinstitute.com/xephem/help/xephem.html.
@@ -51,13 +113,15 @@ def create_catalog_from_edb(name, save_db, edb_file_obj):
              exists then an error will be raised.
     edb_file_obj: either a file or a string. This is the location of the edb
              database. The file is not overwritten.
+    master_db: A MasterDatabase instance, or None if one has to be created.
+             
+    Returns a MasterDatabase instance, either master_db or a new one.     
     """
-    db = tables.openFile(save_db, "a", title="AstroOrganizer Database")
-    try:
-        group = db.getNode("/", "catalogs")
-    except tables.NoSuchNodeError:
-        group = db.createGroup("/", "catalogs", "All the catalogs")
+    if type(master_db) is str:
+        master_db = MasterDatabase(master_db)
+    db = master_db.db
     
+    group = db.getNode("/", "catalogs")
     
     table = db.createTable(group, name, TableBody, "Catalog")
     element = table.row    
@@ -94,4 +158,5 @@ def create_catalog_from_edb(name, save_db, edb_file_obj):
         element['edb_string'] = line
         element.append()
     
-    db.close()    
+    db.flush()
+    return master_db
