f705e89b278152ae72aaad21d1bd1db161c4a972
Lorenzo Riano <lorenzo.riano@gmail.com>
2012-10-08 00:52:12 -0700

    adding new elements

--- astro_organizer/__init__.py
+++ astro_organizer/__init__.py
@@ -4,9 +4,14 @@ import filters
 import string_conversions
 import tour
 import utils
+import graphs
 
 Body = body.Body
 MasterDatabase = catalogs.MasterDatabase
 MultiFilter = filters.MultiFilter
 Tour = tour.Tour
+
 create_date = utils.create_date
+sunrise = utils.sunrise
+sunset = utils.sunset
+copy_observer = utils.copy_observer
