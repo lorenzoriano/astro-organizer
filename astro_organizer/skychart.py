1a1f0f8f5f1ef142bcece2c1f450e35ee103c645
Lorenzo Riano <lorenzo.riano@gmail.com>
2012-10-12 00:19:10 -0700

    minor bugfix for searching

--- astro_organizer/skychart.py
+++ astro_organizer/skychart.py
@@ -1,10 +1,12 @@
 import socket
 import logging
 import datetime
+import time
 
 import catalogs
 import body
 import ephem
+import utils
 
 
 class SkyChartClient(object):
@@ -29,7 +31,9 @@ class SkyChartClient(object):
         return "Not found" in self.__fix_response(msg)    
     
     def __send_and_check(self, msg):
+        #print ("Command: " + msg)
         self.socket.sendall(msg + self.escape_char)
+        time.sleep(1.0)
         res = self.socket.recv(1024)
         
         if self.__is_ok_message(res):
@@ -44,12 +48,14 @@ class SkyChartClient(object):
         if type(body_obj) is body.Body:
             body_obj = body_obj.name
         
+        body_obj = body_obj.replace(" ", "")
         cmd = "SEARCH " + body_obj
+        
         return self.__send_and_check(cmd)
         
     
     def setdate(self, date):
-        assert isinstance(date, datetime.datetime)
+        date = ephem.localtime(utils.create_date(date))
         #yyyy-mm-dd hh:mm:ss
         date_str = "\"%d-%d-%d %d:%d:%d\"" %(date.year, date.month, date.day,
                                          date.hour, date.minute, date.second)
