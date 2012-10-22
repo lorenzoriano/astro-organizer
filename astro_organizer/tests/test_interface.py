16e37bc9f5a57e655918a59dcc3f3cb837492e7a
Lorenzo Riano <lorenzo.riano@gmail.com>
2012-10-21 18:00:44 -0700

    default test

--- astro_organizer/tests/test_interface.py
+++ astro_organizer/tests/test_interface.py
@@ -6,10 +6,10 @@ from astro_organizer import catalogs
 
 def main():
     db = catalogs.MasterDatabase("main_database.h5")
-    m13 = db.find_body("M ")
+    tour = db.get_tour('binocular_objects')
     observer = db.create_observer("Grizzly")
-    t = qt_interface.create_table_from_set(observer, list(m13))
-
+    t = qt_interface.create_table_from_set(observer, tour)
+    
     return qt_interface.app.exec_()
 
 if __name__ == "__main__":
